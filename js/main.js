8cbbd286332a87faf1364472cd023b5a4f4efb63
Raz Amsalem <135437788+razamsalem@users.noreply.github.com>
2023-08-04 13:08:02 +0300

    UNDO,  copy board system and more little features

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 'use strict'
 
-
 const hideRightClick = window.addEventListener("contextmenu", e => e.preventDefault())
 const gElModal = document.querySelector('.modal')
 const gAudio = new Audio("sound/music.mp3")
 
 var gBoard
+var gSavedBoards = [] // for undo
 var gLives
 var gFlagLeft
 var gTimeInterval
-var isMoonToggled = false
+var isMoonToggled = false // for darkmode
 var isMusicPlaying = false
 
 var gLevel = {
@@ -45,22 +45,22 @@ function onInit(level = 'medium') {
         };
         gLives = 3
     }
-    playNewLevelSound()
-    clearInterval(gTimeInterval)
-    gBoard = buildBoard()
-    renderBoard(gBoard)
-    gElModal.classList.add('hidden')
-    gGame.isFirstClick = true
-    gGame.secsPassed = 0
+    playNewLevelSound() //sound for every new game
+    clearInterval(gTimeInterval) //stop timer from last round
+    gBoard = buildBoard() // build the board for the model
+    renderBoard(gBoard) //render the board to the user (DOM)
+    gElModal.classList.add('hidden') // hide modal every new game
+    gGame.isFirstClick = true // first click never will be mine
+    gGame.secsPassed = 0 // restart the timer
     gGame.isOn = true
-    gFlagLeft = gLevel.MINES
-    renderUI()
-    countFlag()
-    updateTimerDisplay()
-    gTimeInterval = setInterval(updateTimer, 1000)
+    gFlagLeft = gLevel.MINES //flags counting the mines
+    renderUI() // render the smiley expressions / hearts according to the life amount
+    countFlag() // to the DOM
+    updateTimerDisplay() // to the DOM
+    gTimeInterval = setInterval(updateTimer, 1000) //start the timer every new game
 }
 
-function buildBoard() {
+function buildBoard() { // building the board for the model
     const board = []
     for (var i = 0; i < gLevel.SIZE; i++) {
         board[i] = []
@@ -77,7 +77,7 @@ function buildBoard() {
     return board
 }
 
-function deployMines(row, col) {
+function deployMines(row, col) { //function that Im using only after the first click
     var minesAmount = gLevel.MINES
     while (minesAmount > 0) {
         const randRow = getRandomInt(0, gLevel.SIZE)
@@ -91,7 +91,7 @@ function deployMines(row, col) {
     setMinesNegsCount(gBoard)
 }
 
-function setMinesNegsCount(board) {
+function setMinesNegsCount(board) { // checks for the mines near the clicked cell
     for (var rowIdx = 0; rowIdx < board.length; rowIdx++) {
         for (var colIdx = 0; colIdx < board[rowIdx].length; colIdx++) {
             var mineCount = 0
@@ -112,8 +112,7 @@ function setMinesNegsCount(board) {
     }
 }
 
-function renderBoard(board) {
-    //DONE: Render the board as a <Table> to the page
+function renderBoard(board) { // render the board for the user (DOM)
     var strHTML = ''
     for (var i = 0; i < gLevel.SIZE; i++) {
         strHTML += '<tr>'
@@ -138,9 +137,26 @@ function renderBoard(board) {
     elContainer.innerHTML = strHTML
 }
 
-function onCellClicked(elCell, i, j) {
+function boardCopy(board) { // copy the current board to use that for undo function
+    var copy = []
+    for (var i = 0; i < board.length; i++) {
+        copy[i] = []
+        for (var j = 0; j < board[i].length; j++) {
+            var cell = board[i][j]
+            copy[i][j] = {
+                minesAroundCount: cell.minesAroundCount,
+                isShown: cell.isShown,
+                isMine: cell.isMine,
+                isMarked: cell.isMarked
+            }
+        }
+    }
+    return copy
+}
+
+function onCellClicked(elCell, i, j) { // the heart of the code // every interaction that the user do 
     if (!gGame.isOn) return
-    if (gGame.isFirstClick) deployMines(i, j)
+    if (gGame.isFirstClick) deployMines(i, j) // after first click its deploy mines
     if (gBoard[i][j].isShown || gBoard[i][j].isMarked) return
 
     playExpandSound()
@@ -149,65 +165,66 @@ function onCellClicked(elCell, i, j) {
     elCell.innerText = gBoard[i][j].isMine ? '💣' : gBoard[i][j].minesAroundCount
 
     if (gBoard[i][j].isMine) {
-        updateFlag()
+        updateFlag() // make its decrease
         gLevel.MINES--
         gLives--
         playDamageSound()
 
-        const heartImg = document.querySelector(`.heart${gLives + 1}-img`)
+        const heartImg = document.querySelector(`.heart${gLives + 1}-img`) //every time the user click on mine 1 heart will be black
         heartImg.src = "img/black-heart.png"
         if (gLives === 2) {
             const smileyImg = document.querySelector('.smiley-img')
-            smileyImg.src = "img/hurt.png"
+            smileyImg.src = "img/hurt.png" // another face to the smiley according the lives left
         }
 
         if (gLives === 1) {
             const smileyImg = document.querySelector('.smiley-img')
-            smileyImg.src = "img/danger.png"
+            smileyImg.src = "img/danger.png" // another face to the smiley according the lives left
         }
 
         if (gLives === 0) {
             playLosingSound()
             const heartImg = document.querySelector(`.heart${gLives + 1}-img`)
             heartImg.src = "img/black-heart.png"
-            gGame.isOn = false
-            msg('Game Over!, Click me to Respawn')
-            clearInterval(gTimeInterval)
-            revealMines()
+            gGame.isOn = false // stop the game
+            msg('Game Over!, Click me to Respawn') // the user get a msg from the smiley
+            clearInterval(gTimeInterval) // stop timer
+            revealMines() // show all the mines after the game over
             const smileyImg = document.querySelector('.smiley-img')
-            smileyImg.src = "img/dead.png"
+            smileyImg.src = "img/dead.png" // another face to the smiley according his death
         }
     }
 
-    if (gBoard[i][j].minesAroundCount === 0) {
-        //DONE:Expand all the neighbors 
+    if (gBoard[i][j].minesAroundCount === 0) { // if the user click on empty cell its gonna expand further
         expandShown(gBoard, elCell, i, j)
     }
-    checkGameOver()
+    gSavedBoards.push(boardCopy(gBoard)) // saving the last board to copy
+    checkWin()
 }
 
 function onCellMarked(elCell, i, j) {
     if (!gGame.isOn) return
     if (gBoard[i][j].isShown) return
     playExpandSound()
-    gBoard[i][j].isMarked = !gBoard[i][j].isMarked
+    gBoard[i][j].isMarked = !gBoard[i][j].isMarked //toggle
 
     if (gBoard[i][j].isMarked) {
         if (gFlagLeft === 0) return
         elCell.innerText = '🚩'
         gGame.markedCount++
-        updateFlag()
+        updateFlag() // decrease the flag count
     } else {
         elCell.innerText = '🟦'
         gGame.markedCount--
-        addFlag()
+        addFlag() // increase back the flag count
     }
     console.log(gGame)
 
-    checkGameOver()
+    gSavedBoards.push(boardCopy(gBoard)) // saving the last board to copy
+    checkWin()
 }
 
-function checkGameOver() {
+function checkWin() { // checking if the user win
     const totalCells = gLevel.SIZE * gLevel.SIZE
     const totalMines = gLevel.MINES
 
@@ -221,7 +238,7 @@ function checkGameOver() {
 
 }
 
-function expandShown(board, elCell, i, j) {
+function expandShown(board, elCell, i, j) { // try to expand further if its empty cell (elCell is not just unused, in the recursion he's gonna be elNeighborCell)
 
     if (gBoard[i][j].isMine) return
 
@@ -233,7 +250,7 @@ function expandShown(board, elCell, i, j) {
             if (rowIdx === i && colIdx === j) continue
 
             var currCell = board[rowIdx][colIdx]
-            
+
 
             if (!currCell.isShown && !currCell.isMarked) {
                 currCell.isShown = true
@@ -248,10 +265,10 @@ function expandShown(board, elCell, i, j) {
             }
         }
     }
-    checkGameOver()
+    checkWin()
 }
 
-function revealMines() {
+function revealMines() { // make the mines reveal after the user dead
     for (var i = 0; i < gLevel.SIZE; i++) {
         for (var j = 0; j < gLevel.SIZE; j++) {
             if (gBoard[i][j].isMine) {
@@ -259,6 +276,15 @@ function revealMines() {
                 elCell.innerText = '💣'
             }
         }
+    }
+}
+
+function makeUndo() { 
+    if (gSavedBoards.length === 0) return
+    gSavedBoards.pop() // to pop the same current board
+    if (gSavedBoards.length > 0) {
+        gBoard = boardCopy(gSavedBoards[gSavedBoards.length - 1]) // gBoard gonna be the last saved board (model)
+        renderBoard(gBoard) // DOM
     }
 }
 
@@ -277,7 +303,7 @@ function updateFlag() { //model
     countFlag()
 }
 
-function addFlag() {
+function addFlag() { // model
     gFlagLeft++
     countFlag()
 }
@@ -287,17 +313,17 @@ function countFlag() { // dom
     elFlagLeft.innerText = gFlagLeft
 }
 
-function msg(text) {
+function msg(text) { // print the user msg
     gElModal.classList.remove('hidden')
     gElModal.innerText = text
 }
 
-function showHelp() {
+function showHelp() { // just a instruction center 
     const elHelp = document.querySelector('.instruction')
     elHelp.classList.toggle('hidden')
 }
 
-function renderUI() {
+function renderUI() { // render the hearts according the lives (and the first smiley)
     const smileyImg = document.querySelector('.smiley-img')
     smileyImg.src = "img/happy.png"
 
@@ -319,13 +345,15 @@ function renderUI() {
     }
 }
 
-function darkMode() {
+function darkMode() { // dark mode features
+    const elTimer = document.querySelector('.timer')
     const elImg = document.querySelector('.smiley img')
     elImg.classList.toggle('border')
+    elTimer.style.color = 'white'
     document.body.classList.toggle('dark-mode')
 }
 
-function onEasy() {
+function onEasy() { // for the difficulties buttons
     onInit('easy')
 }
 
@@ -337,11 +365,11 @@ function onHard() {
     onInit('hard')
 }
 
-function onRestart() {
+function onRestart() { // for the click on the smiley (I found this to be the most efficient way without adding unnecessary bugs)
     window.location.reload()
 }
 
-function toggleMoon(moon) {
+function toggleMoon(moon) { // some fancy Design according to the situation
     if (isMoonToggled) {
         moon.innerText = '🌔'
     } else {
